68b4d3194f2fb4788c109ff9ef4797aad026b3a8
imagine <88944530+imagine1337@users.noreply.github.com>
2022-01-24 17:11:11 -0700

    lol

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests,json
+users=[]
+for user in users:
+    c=requests.post(f'https://en.wikipedia.org/w/api.php?action=query&format=json&list=users&ususers={user}&usprop=cancreate&formatversion=2&errorformat=html&errorsuselocal=true&uselang=en', headers={'content-type':'application/json'})
+    if json.loads(c.text)['query']['users'][0]['cancreate']:
+        print(f'{user} not taken')
